51f6cb5f6fad49d1eeee3756bcea0365c41498cd
wesleysrael <wesley12israel@gmail.com>
2020-10-18 01:22:14 -0300

    completed frontend and started the backend

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -37,8 +37,8 @@ map.on("click", (event) => {
   const lng = event.latlng.lng;
 
   // remover icon
-  document.querySelector("[name=lat]").value(lat);
-  document.querySelector("[name=lng]").value(lng);
+  document.querySelector("[name=lat]").value = lat;
+  document.querySelector("[name=lng]").value = lng;
 
   marker && map.removeLayer(marker);
 
@@ -46,3 +46,62 @@ map.on("click", (event) => {
 
   marker = L.marker([lat, lng], { icon }).addTo(map);
 });
+
+// add campos de fotos
+
+function addPhotoField() {
+  // pegar o container de fotos #images
+  const container = document.querySelector("#images");
+  // pegar o container para duplicar .new-images
+  const fieldsContainer = document.querySelectorAll(".new-upload");
+  // relizar o clone da última imagem adicionada
+  const newFieldContainer = fieldsContainer[
+    fieldsContainer.length - 1
+  ].cloneNode(true);
+
+  // verificar se o campo está vazio, se sim,não adicionar ao container de images
+  const input = newFieldContainer.children[0];
+
+  if (input.value == "") {
+    return;
+  }
+  // limpar o campo antes de adicionar ao container de images
+  input.value = "";
+
+  // adicionar o clone ao container de #images
+  container.appendChild(newFieldContainer);
+}
+
+function deleteField(event) {
+  const span = event.currentTarget;
+
+  const fieldsContainer = document.querySelectorAll(".new-upload");
+
+  if (fieldsContainer.length <= 1) {
+    // limpar o valor do campo
+    span.parentNode.children[0].value = "";
+    return;
+  }
+
+  // deleter o campo
+  span.parentNode.remove();
+}
+
+// seleciona o input
+function toggleSelect(event) {
+  //retirar a classe .active (dos botões)
+  document.querySelectorAll(".button-select button").forEach((button) => {
+    button.classList.remove("active");
+  });
+  // colocar a classe .active
+
+  const button = event.currentTarget;
+  button.classList.add("active");
+
+  // atualizar o meu input hidder
+  const input = document.querySelector('[name="open_on_weekends"]');
+  console.log(input);
+
+  //verificar se sim ou não
+  input.value = button.dataset.value;
+}
